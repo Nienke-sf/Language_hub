365610ee5cf21ac651f94aa0f297fcfdf24220d1
Nienke-sf <preciousngwe2004@gmail.com>
2026-02-12 00:13:01 +0000

    Day 2 - Home page is completed noe creating first language page.

---
+++ jscript/main.js
@@ -0,0 +1,11 @@
+// console.log('main.js loaded');
+//   const toggle = document.getElementById("themeToggle");
+
+
+//   // Toggle theme when clicked
+//   function setLightMode() {
+//     if (this.checked) {
+//       document.documentElement.setAttribute("data-theme", "light");
+//       localStorage.setItem("theme", "light");
+//     } 
+//   }
